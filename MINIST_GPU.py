1ceec5c20f5b1fc33458003b45b3c6cd3f5bff57
dalaomp <32985701+dalaomp@users.noreply.github.com>
2022-09-14 21:48:57 +0800

    Update MINIST_GPU.py

--- MINIST_GPU.py
+++ MINIST_GPU.py
@@ -4,8 +4,6 @@ from torch.autograd import Variable
 import torch.utils.data as Data
 from torchvision import datasets, transforms
 import time
-
-# 创建神经网络
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -28,71 +26,39 @@ class CNN(nn.Module):
         output = self.output_layer(x)
         return output
 
-# 超参数
 EPOCH = 2
 BATCH_SIZE = 100
 LR = 0.001
-DOWNLOAD = False  # 若已经下载mnist数据集则设为False
 
-# 下载mnist数据
-train_data = datasets.MNIST(
-    root='./data',  # 保存路径
-    train=True,  # True表示训练集，False表示测试集
-    transform=transforms.ToTensor(),  # 将0~255压缩为0~1
-    download=DOWNLOAD
-)
+train_data = datasets.MNIST(root='./data',  train=True,  transform=transforms.ToTensor(), download=True)
 
 # DataLoader
-train_loader = Data.DataLoader(
-    dataset=train_data,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    num_workers=2
-)
+train_loader = Data.DataLoader(dataset=train_data,batch_size=BATCH_SIZE,shuffle=True,num_workers=2)
 
-test_data = datasets.MNIST(
-    root='./data',
-    train=False
-)
+test_data = datasets.MNIST(root='./data',train=False)
 
 cnn = CNN()
-# 将神经网络移到GPU上
 cnn.cuda()
 print(cnn)
 
-# 优化器
 optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
 
-# 损失函数
 loss_func = nn.CrossEntropyLoss()
 
-# 为了节约时间，只使用测试集的前2000个数据
-test_x = Variable(
-    torch.unsqueeze(test_data.data, dim=1),
-    volatile=True
-).type(torch.FloatTensor)[:2000] / 255  # 将将0~255压缩为0~1
+# 为了节约时间，只使用测试集前3000个数据
+test_x = Variable(torch.unsqueeze(test_data.data, dim=1),volatile=True).type(torch.FloatTensor)[:3000] / 255 
 
-test_y = test_data.targets[:2000]
-
-# # 使用所有的测试集
-# test_x = Variable(
-#     torch.unsqueeze(test_data.test_data, dim=1),
-#     volatile=True
-# ).type(torch.FloatTensor)/255 # 将将0~255压缩为0~1
-
-# test_y = test_data.test_labels
+test_y = test_data.targets[:3000]
 
 # 将测试数据移到GPU上
 test_x = test_x.cuda()
 test_y = test_y.cuda()
 
-# 开始计时
 start = time.time()
 
 # 训练神经网络
 for epoch in range(EPOCH):
     for step, (batch_x, batch_y) in enumerate(train_loader):
-        # 将训练数据移到GPU上
         batch_x = batch_x.cuda()
         batch_y = batch_y.cuda()
         output = cnn(batch_x)
@@ -102,26 +68,17 @@ for epoch in range(EPOCH):
         optimizer.step()
         if step % 50 == 0:
             test_output = cnn(test_x)
-            # 将预测结果移到GPU上
             predict_y = torch.max(test_output, 1)[1].cuda().data.squeeze()
             accuracy = (predict_y == test_y).sum().item() / test_y.size(0)
             print('Epoch', epoch, '|', 'Step', step, '|', 'Loss', loss.data.item(), '|', 'Test Accuracy', accuracy)
 
 end = time.time()
 
-# 训练耗时
 print('Time cost:', end - start, 's')
 
 # 预测
-test_output = cnn(test_x[:100])
-# 为了将CUDA tensor转化为numpy，需要将数据移回CPU上
-# 否则会报错：TypeError: can't convert CUDA tensor to numpy. Use Tensor.cpu() to copy the tensor to host memory first.
+test_output = cnn(test_x[:10])
 predict_y = torch.max(test_output, 1)[1].cpu().data.numpy().squeeze()
-real_y = test_y[:100].cpu().numpy()
-print(predict_y)
-print(real_y)
-
-# 打印预测和实际结果
-for i in range(10):
-    print('Predict', predict_y[i])
-    print('Real', real_y[i])
+real_y = test_y[:10].cpu().numpy()
+print("预测：",predict_y)
+print("实际：",real_y)
